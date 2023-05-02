eb8d201ba3926e76ead7a745a2ca6216de7462ba
fish0131 <68525727+fish0131@users.noreply.github.com>
2023-05-02 19:30:21 +0800

    Add files via upload

--- 0065. Valid Number/valid_number.cpp
+++ 0065. Valid Number/valid_number.cpp
@@ -7,15 +7,15 @@ public:
             if (s[i] == ' ') {
                 if (i < n - 1 && s[i + 1] != ' ' && (num || dot || exp || sign)) return false;
             } else if (s[i] == '+' || s[i] == '-') {
-                if (i > 0 && s[i - 1] != 'e' && s[i - 1] != ' ') return false;
+                if (i > 0 && s[i - 1] != 'e' && s[i - 1] != 'E' && s[i - 1] != ' ') return false;  // if "+" or "-" is not at the first position, judge whether s is a decimal number or not
                 sign = true;
-            } else if (s[i] >= '0' && s[i] <= '9') {
+            } else if (s[i] >= '0' && s[i] <= '9') {  // judge whether s[i] is a integer or not
                 num = true;
                 numAfterE = true;
             } else if (s[i] == '.') {
-                if (dot || exp) return false;
+                if (dot || exp) return false;  // reject double "." or double "e" or double "E"  ex: "12.3.4" and "1e-07e" are not valid numbers
                 dot = true;
-            } else if (s[i] == 'e' || s[i] == 'E') {
+            } else if (s[i] == 'e' || s[i] == 'E') {  
                 if (exp || !num) return false;
                 exp = true;
                 numAfterE = false;
