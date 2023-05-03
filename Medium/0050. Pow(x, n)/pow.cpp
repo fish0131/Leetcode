e92b29abaacded06256b2e44df03e48e95d6ab38
fish0131 <68525727+fish0131@users.noreply.github.com>
2023-05-03 13:44:56 +0800

    Add files via upload

---
+++ Medium/0050. Pow(x, n)/pow.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) return 1;
+        else if (n == 1) return x;
+        double half = myPow(x, n/2);
+        if (n % 2 == 0) return half * half;
+        else if (n > 0) return half * half * x;
+        else return half * half / x;
+    }
+};
