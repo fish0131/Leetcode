76603d879f7f669e867f7271ce8c6faf703646d5
WEI-YUN HSU <fish3009.cs11@nycu.edu.tw>
2023-06-15 19:24:32 +0800

    Runtime: 0 ms, Memory Usage: 5.9 MB

---
+++ Hard/0065. Valid Number/valid_number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isNumber(string s) {
+        bool num = false, numAfterE = true, dot = false, exp = false, sign = false;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            if (s[i] == ' ') {
+                if (i < n - 1 && s[i + 1] != ' ' && (num || dot || exp || sign)) return false;
+            } else if (s[i] == '+' || s[i] == '-') {
+                if (i > 0 && s[i - 1] != 'e' && s[i - 1] != 'E' && s[i - 1] != ' ') return false;  // if "+" or "-" is not at the first position, judge whether s is a decimal number or not
+                sign = true;
+            } else if (s[i] >= '0' && s[i] <= '9') {  // judge whether s[i] is a integer or not
+                num = true;
+                numAfterE = true;
+            } else if (s[i] == '.') {
+                if (dot || exp) return false;  // reject double "." or "e." or "E."  ex: "12.3.4" and "1e7.5" are not valid numbers
+                dot = true;
+            } else if (s[i] == 'e' || s[i] == 'E') {  
+                if (exp || !num) return false;
+                exp = true;
+                numAfterE = false;
+            } else return false;
+        }
+        return num && numAfterE;
+    }
+};
