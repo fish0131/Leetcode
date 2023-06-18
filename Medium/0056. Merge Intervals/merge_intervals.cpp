31dc5761aaf0ba7b0d30c3e053f1af3d43e9118d
WEI-YUN HSU <fish3009.cs11@nycu.edu.tw>
2023-06-18 11:59:07 +0800

    Runtime: 40 ms, Memory Usage: 18.9 MB

---
+++ Medium/0056. Merge Intervals/merge_intervals.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> res = {intervals[0]};
+        for(int i = 0; i < intervals.size(); i++) {
+            if(res.back()[1] < intervals[i][0])
+                res.push_back(intervals[i]);
+            else
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+        }
+        return res;
+    }
+};
