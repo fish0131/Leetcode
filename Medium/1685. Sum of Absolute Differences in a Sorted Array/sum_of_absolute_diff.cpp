56575afdd89cc70fa9a1b2c08303cfc8b8e003b1
WEI-YUN HSU <fish3009.cs11@nycu.edu.tw>
2023-07-12 09:33:23 +0800

    Runtime: 140 ms, Memory Usage: 83.2 MB

---
+++ Medium/1685. Sum of Absolute Differences in a Sorted Array/sum_of_absolute_diff.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n);
+        // Idea is inspired by Hint3
+        for(int i = 1, sum = 0; i < n; i++)
+            res[i] += (sum += (nums[i] - nums[i-1]) * i);
+        for(int i = n - 2, sum = 0; i >= 0; i--)
+            res[i] += (sum += (nums[i+1] - nums[i]) * (n - i - 1));
+        return res;
+    }
+};
