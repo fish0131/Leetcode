ed105dc4b16253a16ef44fc4b7806a096321def1
WEI-YUN HSU <fish3009.cs11@nycu.edu.tw>
2026-03-11 14:54:47 +0800

    Add files via upload

--- Easy/0014. Longest Common Prefix/longest_common_prefix.cpp
+++ Easy/0014. Longest Common Prefix/longest_common_prefix.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans = "";
-        int min_len = 200; // constraints
-        // find the minimum of string length
-        for (string str: strs) {
-            if (str.size() < min_len) {
-                min_len = str.size();
+        // string ans = "";
+        // int min_len = 200;
+        // for (string str: strs) {
+        //     if (str.size() < min_len) {
+        //         min_len = str.size();
+        //     }
+        // }
+        // for(int i = 0; i < min_len; i++) {
+        //     for (int j = 0; j < strs.size()-1; j++) {
+        //         if (strs[j][i] != strs[j+1][i]) return ans;
+        //     }
+        //     ans += strs[0][i];
+        // }
+        // return ans;
+        
+        for(int i = 0; i < strs[0].size(); i++) {
+            for(int j = 0; j < strs.size() - 1; j++) {
+                if(i >= strs[j].size() || strs[j][i] != strs[j+1][i])
+                    return strs[0].substr(0, i);
             }
         }
-        // compare
-        for(int i = 0; i < min_len; i++) {
-            for (int j = 0; j < strs.size()-1; j++) {
-                if (strs[j][i] != strs[j+1][i]) return ans;
-            }
-            ans += strs[0][i];
-        }
-        return ans;
+        return strs[0];
     }
 };
