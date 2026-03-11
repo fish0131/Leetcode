e1212d2b8af7951663d1beceb8da50e79a2fb538
WEI-YUN HSU <fish3009.cs11@nycu.edu.tw>
2026-03-11 14:26:38 +0800

    Add files via upload

--- Easy/0001. Two Sum/two_sum.cpp
+++ Easy/0001. Two Sum/two_sum.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
-        for (int i = 0; i < nums.size(); i++) {
-            for(int j = i + 1; j < nums.size(); j++) {
-                if(nums[i] + nums[j] == target)
-                    ans = {i, j};
+        // for (int i = 0; i < nums.size(); i++) {
+        //     if(nums[i] >= target) break;
+        //     for(int j = i + 1; j < nums.size(); j++) {
+        //         if(nums[i] + nums[j] == target)
+        //             return {i, j};
+        //     }
+        // }
+        // return {};
+        unordered_map<int, int> map;
+        for(int i = 0; i < nums.size(); i++) {
+            int remain = target - nums[i];
+            if(map.count(remain)) {
+                return {map[remain], i};
             }
+            map[nums[i]] = i;
         }
-        return ans;
+        return {};
     }
 };
